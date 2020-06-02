f400cad0d496f18bfd078c0b82d9f8b75c812e78
its-a-wrap <patrick_tweed@hotmail.co.uk>
2020-06-02 09:37:37 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 
 const lengthOfLastWord = s => {
-  if (!s) return 0;
   const splitWords = s.trim().split(/[ ]+/);
-  return splitWords[splitWords.length - 1].length;
+  return splitWords[splitWords.length - 1].length || 0;
 };
 
 module.exports = lengthOfLastWord;
